0adb3b62797a41698c81383e8c962530d0ffad38
Sourav <sourav@Souravs-MacBook-Pro.local>
2019-10-19 13:15:52 +0800

    l

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const RootStack = createStackNavigator(
     Details: {
       screen: Details,
       navigationOptions: {
-        headerTitle: "Fund Prices"
+        headerTitle: "Shareclass"
       }
     }
   },
